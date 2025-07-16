6fc55b924bb796ed6252ff3db720e1f3cfa0889e
h <haifa9493@gmail.com>
2025-07-16 11:51:58 +0300

    add task 0 and readme

---
+++ debugging/factorial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if len(sys.argv) < 2:
+    print("Error: Please provide a number as an argument.")
+else:
+    f = factorial(int(sys.argv[1]))
+    print(f)
